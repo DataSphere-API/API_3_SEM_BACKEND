474c283315322687b8e8e4d8a9ff682190085f6f
Vincius <viniviniee11@gmail.com>
2026-09-04 19:35:36 -0300

    chore: adiciona os arquivos da aplicação spring

---
+++ src/test/java/com/datasphere/fusex/FusexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasphere.fusex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
