850dddcd19eb91b4b73d839d0fb93fb09db52b25
lucas <lucas.consolo@gmail.com>
2026-09-17 20:30:25 -0300

    feat: criacao de classes para faturamento

---
+++ src/main/java/com/datasphere/fusex/model/ProcedimentoModel.java
@@ -0,0 +1,29 @@
+package com.datasphere.fusex.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "procedimento")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProcedimentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 150, nullable = false)
+    private String nome;
+}
---
+++ src/main/java/com/datasphere/fusex/repository/BeneficiarioRepository.java
@@ -0,0 +1,4 @@
+package com.datasphere.fusex.repository;
+
+public interface BeneficiarioRepository {
+}
