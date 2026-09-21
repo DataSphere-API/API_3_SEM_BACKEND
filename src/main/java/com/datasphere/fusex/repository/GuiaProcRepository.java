e844be9f8dd8c4a6d1a1a4db79851bd528b89aac
mouradaiane <daianemoura_@hotmail.com>
2026-09-21 20:28:32 -0300

    feat: cria a interface GuiaProcRepository para operações CRUD

--- src/main/java/com/datasphere/fusex/repository/GuiaProcRepository.java
+++ src/main/java/com/datasphere/fusex/repository/GuiaProcRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GuiaProcRepository extends JpaRepository<GuiaProc, Long> {
+public interface GuiaProcRepository extends JpaRepository <GuiaProc, Long> {
     GuiaProc findByQr (String qr);
 }
