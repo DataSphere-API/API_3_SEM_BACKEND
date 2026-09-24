2514f0e14a991118b7ffa836c9be1f2d47c320fb
Vincius <viniviniee11@gmail.com>
2026-09-24 20:29:26 -0300

    feat: adiciona responseEntity na resposta das requisições da controller

--- src/main/java/com/datasphere/fusex/atendimento/controller/AtendimentoController.java
+++ src/main/java/com/datasphere/fusex/atendimento/controller/AtendimentoController.java
@@ -1,26 +1,44 @@
 package com.datasphere.fusex.atendimento.controller;
 
+import com.datasphere.fusex.atendimento.service.*;
 import com.datasphere.fusex.guiaProc.GuiaProcModel;
-import com.datasphere.fusex.atendimento.service.AtendimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
 import java.util.List;
 
 @RestController
 public class AtendimentoController {
 
-    @Autowired
-    private AtendimentoService atendimentoService;
+    private RegistrarRealizacaoService registrarRealizacaoService;
+    private ListarAtendimentosPorOcs listarAtendimentosPorOcs;
+
+    public AtendimentoController(RegistrarRealizacaoService registrarRealizacaoService, ListarAtendimentosPorOcs listarAtendimentosPorOcs) {
+        this.registrarRealizacaoService = registrarRealizacaoService;
+        this.listarAtendimentosPorOcs = listarAtendimentosPorOcs;
+    }
 
     @PostMapping ("/atendimentos/validar-qrcode")
-    public GuiaProcModel validarQrCode (@RequestBody String qr) {
-        return atendimentoService.registrarRealizacao(qr);
+    public ResponseEntity<String> validarQrCode (@RequestBody String qr) {
+        registrarRealizacaoService.registrarRealizacao(qr);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("qr code validado");
+    }
+
+    @GetMapping ("/ocs/{cnpj}/atendimentos")
+    public ResponseEntity<?> ListarAtendimentosPorOcs (@PathVariable String cnpj) {
+        List<GuiaProcModel> guiaProcModelList = listarAtendimentosPorOcs.listarAtendimentosPorOcs(cnpj);
+        if (guiaProcModelList == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("nenhum atendimento encontrado");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(guiaProcModelList);
             }
-
-            @GetMapping ("/ocs/{cnpj}/atendimentos")
-                    public List<GuiaProcModel> ListarAtendimentosPorOcs (@PathVariable String cnpj) {
-            return  atendimentoService.listarAtendimentosPorOcs(cnpj);
-
         }
     }
