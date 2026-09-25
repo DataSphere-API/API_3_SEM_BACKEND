c7ed46c491629dc4a66013c8828eb353095acd98
Vincius <viniviniee11@gmail.com>
2026-09-24 22:20:54 -0300

    feat: adiciona o método de encontrar por cnpj da ocs

--- src/main/java/com/datasphere/fusex/guiaProc/GuiaProcRepository.java
+++ src/main/java/com/datasphere/fusex/guiaProc/GuiaProcRepository.java
@@ -3,7 +3,12 @@ package com.datasphere.fusex.guiaProc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GuiaProcRepository extends JpaRepository <GuiaProcModel, Long> {
     GuiaProcModel findByQr (String qr);
+
+    List<GuiaProcModel> findByContratoModel_OcsModel_Cnpj(String cnpj);
+
 }
