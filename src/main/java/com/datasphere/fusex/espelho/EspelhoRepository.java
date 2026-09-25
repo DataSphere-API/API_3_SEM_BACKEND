05042a96c74424e51bbecc3864a227dee69308b6
matquirin0 <contato.matheuspquirino@gmail.com>
2026-09-25 16:48:24 -0300

    feature(US001-5): adiciona findByOcsId e findByStatus, findByEspelhoId e findByIdAndEspelhoId.

--- src/main/java/com/datasphere/fusex/espelho/EspelhoRepository.java
+++ src/main/java/com/datasphere/fusex/espelho/EspelhoRepository.java
@@ -3,9 +3,12 @@ package com.datasphere.fusex.espelho;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EspelhoRepository extends JpaRepository<EspelhoModel, Integer> {
     Optional<EspelhoModel> findByAtendimentoId(Long atendimentoId);
+    List<EspelhoModel> findByOcsId(String ocsId);
+    List<EspelhoModel> findByStatus(StatusEspelho status);
 }
--- src/main/java/com/datasphere/fusex/espelhoItem/EspelhoItemRepository.java
+++ src/main/java/com/datasphere/fusex/espelhoItem/EspelhoItemRepository.java
@@ -3,6 +3,11 @@ package com.datasphere.fusex.espelhoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EspelhoItemRepository extends JpaRepository<EspelhoItemModel, Integer> {
+    List<EspelhoItemModel> findByEspelhoId(Integer espelhoId);
+    Optional<EspelhoItemModel> findByIdAndEspelhoId(Integer id, Integer espelhoId);
 }
